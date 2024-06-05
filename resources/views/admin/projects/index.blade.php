fcd8ce776a1d2f6ae030c5bb43fe95a2b9c7b7c8
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-05 17:27:10 +0200

    ADD: relations between projects and types tables defined in both models + EDIT index view to include type among the info shown on page

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,23 +3,25 @@
 @section('title', 'Projects')
 
 @section('content')
-<div class="container py-5">
+<div class="container">
   <h1 class="text-center p-3">My Projects</h1>
 <table class="table table-hover">
   <thead>
     <tr>
       <th scope="col">Name</th>
-      <th scope="col">Description</th>
-      <th scope="col">Link</th>
+      <th scope="col">Type</th>
+      <th scope="col">Github Link</th>
     </tr>
   </thead>
   <tbody>
   @foreach ($projects as $project )
   <tr>
-      <th scope="row">{{$project->title}}</th>
-      <td>{{$project->description}}</td>
-      <td><a href="{{$project->url}}">link</a></td>
+      <th scope="row"><a href="{{route('admin.projects.show', $project)}}">{{$project->title}}</a></th>
+      <td>{{$project->type ? $project->type->name : "none"}}</td>
+      <td><a href="{{$project->url}}">Link</a></td>
     </tr>
   @endforeach
   </tbody>
+</table>
+@endsection
  
